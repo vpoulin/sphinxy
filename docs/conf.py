db9b6dad6fa08ace9c0f01f8d6b71951e481f012
Valerie Poulin <vpoulin@gmail.com>
2023-04-20 20:53:34 +0000

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -22,11 +22,12 @@ extensions = [
 autoclass_content = 'both'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_favicon = "_static/favicon.png"
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
