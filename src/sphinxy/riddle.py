8e08d9f6c022b8833309257e556d14cd60afd2b9
Valerie Poulin <vpoulin@gmail.com>
2023-04-20 20:15:22 +0000

    Added doc string

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -12,3 +12,19 @@ class Riddle:
 
     def get_hint(self) -> Iterator[str]:
         yield from iter(self.answer)
+        
+    def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should
+                be returned. Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
+
